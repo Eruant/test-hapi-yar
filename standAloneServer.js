373c5e21233b6fdbd46f33c5b590fcce287ecd46
Matt Gale <matt@littleball.co.uk>
2016-09-07 10:46:41 +0100

    Added stand alone server

---
+++ standAloneServer.js
@@ -0,0 +1,51 @@
+const Hapi = require('hapi')
+const yar = require('yar')
+
+const server = new Hapi.Server()
+
+server.connection({
+  port: 3001
+})
+
+server.register({
+  register: yar,
+  options: {
+    cookieOptions: {
+      password: 'afjrg%fica###gsigv0scjedgkGYTTTv',
+      isSecure: false
+    },
+    cache: {
+      expiresIn: 7 * 24 * 60 * 60 * 1000 // 7 days
+    }
+  }
+})
+  .then(() => {
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler (request, reply) {
+        const serverOne = request.yar.get('serverOne') || {visits: 0}
+        const replyMessage = `Visit number: ${serverOne.visits}`
+
+        request.yar.set('serverOne', {
+          visits: serverOne.visits + 1
+        })
+        reply(`<!doctype html>
+          <h1>Stand alone server</h1>
+          <p>${replyMessage}</p>
+          <a href='https://codepen.io/anon/pen/EgjKVA'>Go to codepen</a>
+        `)
+      }
+    })
+
+    server.start((error) => {
+      if (error) {
+        throw error
+      }
+
+      console.log(`Server started at ${server.info.uri}`)
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
